58859b3376f738a42e4f934afccd43b2a4ba42d5
marinasofia <marinasofiaml@gmail.com>
2026-03-13 14:28:11 -0400

    use env var for API URL

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,7 +5,7 @@ import {
   XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend
 } from "recharts";
 
-const API = "http://localhost:5001";
+const API = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001"
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
